62ef2668bff40395776a39bf14cd68526e438c56
minagishl <mvxd568.co@icloud.com>
2024-02-08 02:27:49 +0900

    Add padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,10 +20,10 @@ export default function Home() {
   }, []);
 
   return (
-    <>
+    <div className="p-5">
       {ResponseJSON.map((plop, index) => {
         return <SummaryCardComp plop={plop} key={index} />;
       })}
-    </>
+    </div>
   );
 }
