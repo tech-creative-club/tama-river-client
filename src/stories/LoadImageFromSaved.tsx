15a0bff7698987e27917afbf0b3fc69b718ada05
minagishl <mvxd568.co@icloud.com>
2024-02-08 01:50:36 +0900

    Add QR loading page

--- src/stories/LoadImageFromSaved.tsx
+++ src/stories/LoadImageFromSaved.tsx
@@ -2,8 +2,8 @@ import { Label } from './Label';
 
 export const LoadImageFromSaved = () => {
   return (
-    <div className="m-5 inline-flex items-center justify-center gap-2.5 rounded-full bg-zinc-200 px-5 py-0.5">
-      <Label innerText="保存した画像から読み取る" size="quaternary" weight="normal" />
+    <div className="inline-flex items-center justify-center gap-2.5 rounded-full bg-zinc-200 px-5 py-0.5">
+      <Label innerText="保存した画像から読み取る" size="quaternary" weight="medium" />
     </div>
   );
 };
