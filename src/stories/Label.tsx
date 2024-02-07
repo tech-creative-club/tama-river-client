6764321f829d4e8ca36c23638ae2bc459099da1e
minagishl <mvxd568.co@icloud.com>
2024-02-08 01:21:23 +0900

    Updated to allow resizing of labels

--- src/stories/Label.tsx
+++ src/stories/Label.tsx
@@ -1,22 +1,57 @@
 import React from 'react';
 import { Noto_Sans_JP } from 'next/font/google';
 import Text from '@/components/Text';
-import { twMerge } from 'tailwind-merge';
+import { tv } from 'tailwind-variants';
 
 const NotoSansJP = Noto_Sans_JP({ subsets: ['latin'] });
 
-const base = 'text-center text-sm font-semibold text-zinc-900 text-black';
+const label = tv({
+  base: 'text-center text-sm font-medium',
+  variants: {
+    size: {
+      primary: 'text-base',
+      secondary: 'text-sm',
+      tertiary: 'text-xs',
+      quaternary: 'text-2xs',
+    },
+    weight: {
+      thin: 'font-thin',
+      extralight: 'font-extralight',
+      light: 'font-light',
+      normal: 'font-normal',
+      medium: 'font-medium',
+      semibold: 'font-semibold',
+      bold: 'font-bold',
+      extrabold: 'font-extrabold',
+      black: 'font-black',
+    },
+  },
+});
 
 interface LabelProps {
   innerText: string;
   className?: string;
+  size?: Size;
+  weight?: Weight;
 }
 
-export const Label = ({ innerText, className }: LabelProps) => {
+type Size = 'primary' | 'secondary' | 'tertiary' | 'quaternary';
+type Weight =
+  | 'thin'
+  | 'extralight'
+  | 'light'
+  | 'normal'
+  | 'medium'
+  | 'semibold'
+  | 'bold'
+  | 'extrabold'
+  | 'black';
+
+export const Label = ({ innerText, className, size = 'primary', weight = 'medium' }: LabelProps) => {
   const containsAlphabet = /[a-zA-Z]/.test(innerText);
 
   return (
-    <div className={`${twMerge(base, className)} ${NotoSansJP.className}`}>
+    <div className={`${label({ className, size, weight })} ${NotoSansJP.className}`}>
       {containsAlphabet ? <Text>{innerText}</Text> : innerText}
     </div>
   );
