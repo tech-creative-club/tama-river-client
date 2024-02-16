75a38d275f5b028047e48cbdce348923299d07a6
minagishl <mvxd568.co@icloud.com>
2024-02-17 01:07:14 +0900

    Fix grid class

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -11,8 +11,8 @@ export default function Page() {
       <div className="hidden-scrollbar size-full max-w-7xl overflow-scroll p-5 pb-0">
         <div className="relative flex size-full flex-col">
           <h2 className="text-2xl font-semibold">設定</h2>
-          <div className="grid size-full max-w-7xl grid-cols-2 gap-8 py-5 md:grid-cols-3">
-            <div className=" rounded-lg border-2 p-5">
+          <div className="grid size-full max-w-7xl grid-rows-2 gap-8 py-5 md:grid-cols-3">
+            <div className="w-full rounded-lg border-2 p-5">
               <h2 className="pb-5 text-xl font-semibold">ローカルストレージの削除</h2>
               <p className="text-sm text-secondary-foreground">
                 これを実行するとローカルストレージ内のキーが全て削除されます。
