fecbe384cae651c1878d0cf0adcfe06d7af4d3c9
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-17 00:29:44 +0900

    favのreturnをなんとかした

--- src/app/favorite/page.tsx
+++ src/app/favorite/page.tsx
@@ -9,15 +9,14 @@ import fetchEvents from '@/utils/fetchEvents';
 export default function Favorite() {
   const [ResponseJSON, setResponseJSON] = useState<SummaryCardType[]>([]);
   const [Loading, setLoading] = useState(true);
-  const [favoritesCount, setFavoritesCount] = useState<number>(0);
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetchEvents();
       setResponseJSON(response as SummaryCardType[]);
+      console.log(response);
       setLoading(false);
     }
-
     fetchData();
   }, []);
 
@@ -25,14 +24,11 @@ export default function Favorite() {
     <div className="flex size-full flex-col items-center space-y-5 pt-20">
       {Loading ? (
         <h2>Loading...</h2>
-      ) : favoritesCount === 0 ? (
+      ) : ResponseJSON.length === 0 ? (
         <h2>お気に入りがありません</h2>
       ) : (
         ResponseJSON.map((prop, index) => {
-          if (getFavoriteStorage().includes(prop.url)) {
-            setFavoritesCount(favoritesCount + 1);
-            return <SummaryCard prop={prop} key={index} pulse={Loading} />;
-          }
+          return <SummaryCard prop={prop} key={index} pulse={Loading} />;
         })
       )}
     </div>
