c08e377690b11958f87da5c84d8ad40e8e797748
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-18 00:04:44 +0900

    SummaryCardのstoryを追加

--- src/stories/SummaryCard.stories.ts
+++ src/stories/SummaryCard.stories.ts
@@ -31,7 +31,8 @@ const meta: Meta = {
 } satisfies Meta<typeof SummaryCard>;
 
 export default meta;
-export const Primary: StoryObj<typeof meta> = {
+
+export const Mobile: StoryObj<typeof meta> = {
   args: {
     prop: {
       title: 'イベントの名前',
@@ -47,5 +48,62 @@ export const Primary: StoryObj<typeof meta> = {
     } as SummaryCardProp,
     loading: false,
     desktop: false,
+  },
+};
+
+export const Desktop: StoryObj<typeof meta> = {
+  args: {
+    prop: {
+      title: 'イベントの名前',
+      sport: ['サッカー', '野球'],
+      date: '2024-01-01T00:00:00Z',
+      url: 'https://example.com',
+      image_url: sampleImageFile.src,
+      location: {
+        name: '会場の名前',
+        address: '住所',
+        capacity: '100',
+      },
+    } as SummaryCardProp,
+    loading: false,
+    desktop: true,
+  },
+};
+
+export const LoadingMobile: StoryObj<typeof meta> = {
+  args: {
+    prop: {
+      title: 'イベントの名前',
+      sport: ['サッカー', '野球'],
+      date: '2024-01-01T00:00:00Z',
+      url: 'https://example.com',
+      image_url: sampleImageFile.src,
+      location: {
+        name: '会場の名前',
+        address: '住所',
+        capacity: '100',
+      },
+    } as SummaryCardProp,
+    loading: true,
+    desktop: false,
+  },
+};
+
+export const LoadingDesktop: StoryObj<typeof meta> = {
+  args: {
+    prop: {
+      title: 'イベントの名前',
+      sport: ['サッカー', '野球'],
+      date: '2024-01-01T00:00:00Z',
+      url: 'https://example.com',
+      image_url: sampleImageFile.src,
+      location: {
+        name: '会場の名前',
+        address: '住所',
+        capacity: '100',
+      },
+    } as SummaryCardProp,
+    loading: true,
+    desktop: true,
   },
 };
