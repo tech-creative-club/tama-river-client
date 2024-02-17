57a2213005520a138dda09e071062b344ff79c7a
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-17 13:41:09 +0900

    tagsにオプショナルチェイニングでエラー回避

--- src/stories/TagButton.tsx
+++ src/stories/TagButton.tsx
@@ -11,16 +11,13 @@ export const TagButton = (props: TagButtonProps) => {
   const { tags, selectedTag, onClick } = props;
   return (
     <>
-      <Button onClick={() => onClick('すべて')} active={selectedTag === 'すべて'}>
-        <Label innerText="すべて" size="secondary" weight="medium" />
-      </Button>
-      {/* {tags.map((tag, index) => {
+      {tags?.map((tag, index) => {
         return (
           <Button key={index} onClick={() => onClick(tag)} active={selectedTag === tag}>
             <Label innerText={tag as string} size="secondary" weight="medium" />
           </Button>
         );
-      })} */}
+      })}
     </>
   );
 };
