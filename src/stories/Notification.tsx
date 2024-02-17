b301471972d5494705e4debdac986b61767dab94
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-17 11:07:40 +0900

    NotificationComponentを作成

--- src/stories/Notification.tsx
+++ src/stories/Notification.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box } from "@mui/material";
+import { Box, Card } from "@mui/material";
 import ErrorIcon from '@mui/icons-material/Error';
 import WarningIcon from '@mui/icons-material/Warning';
 import HelpCenterIcon from '@mui/icons-material/HelpCenter';
@@ -12,7 +12,7 @@ export interface NotificationProps {
 }
 
 type NotificationType = 'info' | 'warning' | 'error' | 'success';
-type NotificationIconComponentsType = Record<NotificationType, React.ComponentType>;
+type NotificationIconComponentsType = Record<NotificationType, React.ElementType>;
 
 const NotificationIconComponents: NotificationIconComponentsType = {
   info: HelpCenterIcon,
@@ -30,24 +30,29 @@ function NotificationTitle({title, type}: NotificationTitleProps) {
   //TODO: Icon＋titleの形にする。
   const Icon = NotificationIconComponents[type];
   return (
-    <Box className="flex items-start space-x-1">
-      <Icon />
-      <Box>{title}</Box>
+    <Box className="flex items-start space-x-1 text-2xl">
+      <Icon fontSize="large"/>
+      <Box>
+        <span>{title}</span>
+      </Box>
     </Box>
   );
 }
 
 function NotificationText({text}: {text: string}) {
   return (
-    <p>{text}</p>
+    <p className="text-gray-500">{text}</p>
   );
 }
 
 export function Notification(props: NotificationProps) {
   return (
-    <Box className="flex flex-col space-y-1.5">
-      <NotificationTitle title={props.title} type={props.notificationType} />
-      <NotificationText text={props.text} />
-    </Box>
+
+    <Card className="m-3 p-3" variant="outlined">
+      <Box className="flex flex-col space-y-1.5 p-3">
+        <NotificationTitle title={props.title} type={props.notificationType} />
+        <NotificationText text={props.text} />
+      </Box>
+    </Card>
 );
 };
