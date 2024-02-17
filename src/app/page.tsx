a9cada42a8e9a3333ed83239a2ea69d356d8e653
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-17 15:09:15 +0900

    RenderComponentをDesktop用とMobile用で分けた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,43 @@ import { TagButton } from '@/stories/TagButton'; // TODO: storiesからimportす
 import { SummaryCardType } from '@/stories/SummaryCard';
 import { Notification } from '@/stories/Notification';
 
+type DeviceType = 'mobile' | 'desktop';
+
 interface RenderComponentProps {
   summaryCardJSON: SummaryCardType[];
   Loading: boolean;
   tags: string[];
   selectedTag: string;
   setTag: (tagStr: string) => void;
+  device: DeviceType;
 }
 
-function RenderComponent(props: RenderComponentProps) {
+const deviceType = {
+  mobile: 'mobile',
+  desktop: 'desktop',
+};
+
+function DesktopRenderComponent(props: RenderComponentProps) {
   const { summaryCardJSON, Loading, tags, selectedTag, setTag } = props;
-  // 共通描画責務を持つコンポーネント
+  return (
+    <>
+      <div className="w-full max-w-7xl p-5 pb-0">
+        <Notification title="開発中のお知らせ" text="現在開発中の画面のため、本番とは違う可能性があります。" notificationType="warning"/>
+        <TagButton tags={tags} selectedTag={selectedTag} onClick={(str) => setTag(str)} variant="normal" />
+      </div>
+      <div className="hidden h-fit w-full max-w-7xl grid-cols-4 gap-4 md:grid">
+        {summaryCardJSON.map((prop, index) => {
+          if (selectedTag === 'すべて' || prop.tag[0].name === selectedTag) {
+            return <SummaryCard prop={prop} key={index} loading={Loading} desktop={true} />;
+          }
+        })}
+      </div>
+    </>
+  );
+};
+
+function MobileRenderComponent(props: RenderComponentProps) {
+  const { summaryCardJSON, Loading, tags, selectedTag, setTag } = props;
   return (
     <div className="size-full max-w-md">
       <div className="flex flex-col space-y-5 pb-5">
@@ -38,7 +64,16 @@ function RenderComponent(props: RenderComponentProps) {
       </div>
     </div>
   );
-}
+};
+
+function RenderComponent(props: RenderComponentProps) {
+  const { device } = props;
+  return (
+    <>
+      {device === deviceType.mobile ? <MobileRenderComponent {...props} /> : <DesktopRenderComponent {...props} />}
+    </>
+  );
+};
 
 export default function Home() {
   const [summaryCardJSON, setSummaryCardJSON] = useState<SummaryCardType[]>([]);
@@ -63,26 +98,16 @@ export default function Home() {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
+  const props = { summaryCardJSON, Loading, tags, selectedTag, setTag };
   return (
     <main className="flex-1 overflow-scroll">
       {/* スマホ&タブレット用 */}
       <div className="flex size-full justify-center space-y-5 p-2 md:hidden">
-        <RenderComponent summaryCardJSON={summaryCardJSON} Loading={Loading} tags={tags} selectedTag={selectedTag} setTag={setTag} />
+        <RenderComponent device='mobile' {...props} />
       </div>
       {/* PC用 */}
       <div className="hidden size-full flex-col items-center md:flex">
-        <div className="w-full max-w-7xl p-5 pb-0">
-          <Notification title="開発中のお知らせ" text="現在開発中の画面のため、本番とは違う可能性があります。" notificationType="warning"/>
-          <TagButton tags={tags} selectedTag={selectedTag} onClick={(str) => setTag(str)} variant="normal" />
-        </div>
-        <div className="hidden h-fit w-full max-w-7xl grid-cols-4 gap-4 md:grid">
-          {summaryCardJSON.map((prop, index) => {
-            if (selectedTag === 'すべて' || prop.tag[0].name === selectedTag) {
-              return <SummaryCard prop={prop} key={index} loading={Loading} desktop={true} />;
-            }
-          })}
-        </div>
+        <RenderComponent device='desktop' {...props} />
       </div>
     </main>
   );
