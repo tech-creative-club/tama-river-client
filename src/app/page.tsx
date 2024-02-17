6866ac88d1f352a176a62c9b203845003c390d29
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-17 11:12:41 +0900

    Notificationを表示させることに成功

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Card } from '@/stories/Card';
 import { Label } from '@/stories/Label';
 import { TagButton } from '@/stories/TagButton';
 import SummaryCardType from '@/types/SummaryCardType';
+import { Notification } from '@/stories/Notification';
 
 export default function Home() {
   const [summaryCardJSON, setSummaryCardJSON] = useState<SummaryCardType[]>([]);
@@ -37,17 +38,7 @@ export default function Home() {
       <div className="flex size-full justify-center space-y-5 p-2 md:hidden">
         <div className="size-full max-w-md">
           <div className="flex flex-col space-y-5 pb-5">
-            <Card>
-              <div className="flex flex-col space-y-1.5">
-                <Label innerText="注意！" weight="medium" size="primary" />
-                <Label
-                  innerText="開発中の画面のため、リリース版とは一部使用が異なる場合がございます。"
-                  weight="normal"
-                  size="secondary"
-                  className="text-muted-foreground"
-                />
-              </div>
-            </Card>
+            <Notification title="開発中のお知らせ" text="現在開発中の画面のため、本番とは違う可能性があります。" notificationType="warning"/>
             <div className="p-5 pt-0">
               <div className="hidden-scrollbar w-full overflow-scroll">
                 <div className="flex flex-row space-x-3">
@@ -68,6 +59,7 @@ export default function Home() {
       {/* PC用 */}
       <div className="hidden size-full flex-col items-center md:flex">
         <div className="hidden-scrollbar w-full max-w-7xl overflow-scroll p-5 pb-0">
+          <Notification title="開発中のお知らせ" text="現在開発中の画面のため、本番とは違う可能性があります。" notificationType="warning"/>
           <div className="flex flex-row space-x-3">
             <TagButton tags={tags} selectedTag={selectedTag} onClick={(str) => setTag(str)} />
           </div>
