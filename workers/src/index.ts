bb1c24e670c536fe9580814eca10a73a84103e58
みなぎし <mvxd568.co@icloud.com>
2024-02-22 23:26:53 +0900

    Adding Workers and creating API points

---
+++ workers/src/index.ts
@@ -0,0 +1,44 @@
+import { KVNamespace } from '@cloudflare/workers-types';
+import { Hono } from 'hono';
+
+type Bindings = {
+  MY_KV: KVNamespace;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get('/', async (c) => {
+  return c.text('Hello, world!');
+});
+
+app.post('/api/items', async (c) => {
+  try {
+    const { title, sport, tag, date, url, image_url, location } = await c.req.json();
+
+    if (
+      typeof title === 'string' &&
+      Array.isArray(sport) &&
+      typeof tag === 'object' &&
+      typeof tag.name === 'string' &&
+      typeof date === 'string' &&
+      typeof url === 'string' &&
+      typeof image_url === 'string' &&
+      typeof location === 'object' &&
+      typeof location.name === 'string' &&
+      typeof location.address === 'string' &&
+      typeof location.capacity === 'number'
+    ) {
+      await c.env.MY_KV.put(
+        title,
+        JSON.stringify({ title, sport, tag: [tag], date, url, image_url, location: {} })
+      );
+      return c.json({ success: true });
+    } else {
+      return c.json({ error: 'Invalid request body format' }, { status: 400 });
+    }
+  } catch (error) {
+    return c.json({ error: 'Invalid JSON payload' }, { status: 400 });
+  }
+});
+
+export default app;
