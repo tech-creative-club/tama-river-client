40e6e498beb6e0f488abc4c4a98a3f38d9644ea8
minagishl <mvxd568.co@icloud.com>
2024-02-23 14:47:25 +0900

    Add varidation

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -1,5 +1,7 @@
 import { KVNamespace } from '@cloudflare/workers-types';
 import { Hono } from 'hono';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
 import { Items } from './types/items';
 
 type Bindings = {
@@ -8,35 +10,39 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
+const validator = zValidator(
+  'json',
+  z.object({
+    title: z.string(),
+    sport: z.array(z.string()),
+    tag: z.object({
+      name: z.string(),
+    }),
+    date: z.string(),
+    url: z.string(),
+    image_url: z.string(),
+    location: z.object({
+      name: z.string(),
+      address: z.string(),
+      capacity: z.union([z.string(), z.number()]),
+    }),
+  })
+);
+
 app.get('/', async (c) => {
   return c.text('Hello, world!');
 });
 
-app.post('/api/items', async (c) => {
+app.post('/api/items', validator, async (c) => {
   try {
     const { title, sport, tag, date, url, image_url, location } = (await c.req.json()) as Items;
-    if (
-      typeof title === 'string' &&
-      Array.isArray(sport) &&
-      typeof tag === 'object' &&
-      typeof tag.name === 'string' &&
-      typeof date === 'string' &&
-      typeof url === 'string' &&
-      typeof image_url === 'string' &&
-      typeof location === 'object' &&
-      typeof location.name === 'string' &&
-      typeof location.address === 'string' &&
-      (typeof location.capacity === 'number' || typeof location.capacity === 'string')
-    ) {
-      const data = JSON.stringify({ title, sport, tag, date, url, image_url, location });
-      try {
-        await c.env.TAMARIVER_KV.put(url, data);
-        return c.json({ success: true });
-      } catch (error) {
-        return c.json({ error: 'Internal server error' }, { status: 500 });
-      }
-    } else {
-      return c.json({ error: 'Invalid request body format' }, { status: 400 });
+
+    const data = JSON.stringify({ title, sport, tag, date, url, image_url, location });
+    try {
+      await c.env.TAMARIVER_KV.put(url, data);
+      return c.json({ success: true });
+    } catch (error) {
+      return c.json({ error: 'Internal server error' }, { status: 500 });
     }
   } catch (error) {
     return c.json({ error: 'Invalid JSON payload' }, { status: 400 });
