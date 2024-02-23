ebaffc31e2a0d5f2a485384d52d7a1c0935b1cf3
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-23 13:31:52 +0900

    add worker todo

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -14,7 +14,7 @@ app.get('/', async (c) => {
 app.post('/api/items', async (c) => {
   try {
     const { title, sport, tag, date, url, image_url, location } = await c.req.json();
-
+    // TODO: 型ちゃんと書いてバリデーションする
     if (
       typeof title === 'string' &&
       Array.isArray(sport) &&
@@ -29,6 +29,8 @@ app.post('/api/items', async (c) => {
       (typeof location.capacity === 'number' || typeof location.capacity === 'string')
     ) {
       const data = JSON.stringify({ title, sport, tag, date, url, image_url, location });
+      // TODO: putでerrorが返ってきた時に500エラーを返す。
+      // TODO: keyはtitleではなくurlを使う (e.g. url::example.com/kankou/sport.html のようなKeyを入れる)
       await c.env.TAMARIVER_KV.put(title, data);
       return c.json({ success: true });
     } else {
@@ -38,5 +40,12 @@ app.post('/api/items', async (c) => {
     return c.json({ error: 'Invalid JSON payload' }, { status: 400 });
   }
 });
+
+// TODO: GET endpoint /api/itemsで記事情報のkv全件取得(key prefixが "url" であるものを取得する)
+
+// TODO: GET endpoint /api/image/?url=...で画像を取得するAPIを作成
+// (enhance) (e.g. /api/image/?url=example.com/image.png のようなクエリパラメータを受け取り、BASE64で返す)
+// TODO: POST endpoint /api/image で画像をアップロードするAPIを作成
+// (enhance) (e.g. keyは image::example.com/image.png のようにし、POSTデータでBASE64文字列を送信し、BASE64で保存する)
 
 export default app;
