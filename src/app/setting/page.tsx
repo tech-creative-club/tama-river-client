4ef18e19f2bded86243be9ed35d921a39bc6c719
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-24 14:10:32 +0900

    ストレージリセットの文言修正

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -13,9 +13,9 @@ export default function Page() {
           <h2 className="text-2xl font-semibold">設定</h2>
           <div className="grid size-full max-w-7xl grid-rows-2 gap-8 py-5 md:grid-cols-3">
             <div className="w-full rounded-lg border-2 p-5">
-              <h2 className="pb-5 text-xl font-semibold">ローカルストレージの削除</h2>
+              <h2 className="pb-5 text-xl font-semibold">タマリバユーザデータのリセット</h2>
               <p className="text-sm text-secondary-foreground">
-                これを実行するとローカルストレージ内のキーが全て削除されます。
+                これを実行するといいね情報などが全てリセットされます。
               </p>
               <button
                 className="mt-5 w-full rounded-lg bg-primary p-3 font-semibold text-white"
