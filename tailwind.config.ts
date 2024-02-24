a4cce37dc1fae57db0404899f13213d89b7fcadc
minagishl <mvxd568.co@icloud.com>
2024-02-24 23:26:25 +0900

    Fixed issue with footer not displaying properly in Safari on iPhone

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -86,6 +86,18 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      minHeight: {
+        screen: '100dvh',
+      },
+      height: {
+        screen: '100dvh',
+      },
+      minWidth: {
+        screen: '100dvw',
+      },
+      width: {
+        screen: '100dvw',
+      },
     },
   },
   plugins: [],
