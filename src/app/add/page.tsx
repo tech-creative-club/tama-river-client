fce8d8dadadaa3d207499a27e50701ce2a7079c7
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-25 07:38:29 +0900

    実実装部分を警告

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,10 +1,13 @@
 import { CameraActivation } from '@/components/page/CameraActivation';
 import { LoadImageFromSaved } from '@/components/misc/LoadImageFromSaved';
 import { Label } from '@/components/common/Label';
+import { Notification } from '@/components/common/Notification';
 
 export default function Add() {
+  const errorText = 'この機能はv0.1.0では未実装です。';
   return (
     <div className="flex size-full flex-col items-center space-y-5 pt-20">
+      <Notification title="未実装" text={errorText} notificationType="error" />
       <CameraActivation />
       <LoadImageFromSaved />
       <Label tag="p" className="max-w-64">
