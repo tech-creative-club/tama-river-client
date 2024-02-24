fd658890d94b3f9469380d249cfd563d4f12be6f
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-25 07:48:19 +0900

    検索部分にヘルプを追加

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,11 +3,14 @@
 import { useEffect, useState } from 'react';
 import { SummaryCard, SummaryCardProp } from '@/components/common/SummaryCard';
 import TextField from '@mui/material/TextField';
+import { CircularProgress } from '@mui/material';
+import { Notification } from '@/components/common/Notification';
 
 export default function Search() {
   const [ResponseJSON, setResponseJSON] = useState<SummaryCardProp[]>([]);
   const [Loading, setLoading] = useState(true);
   const [searchText, setSearchText] = useState<string>('');
+  const notificationText = `検索欄にキーワードを入力すると、タイトルにキーワードが含まれる記事を検索できます。`;
 
   useEffect(() => {
     async function fetchData() {
@@ -22,6 +25,7 @@ export default function Search() {
   return (
     <div className="flex size-full flex-col items-center space-y-5 pt-20">
       <div className="w-full px-5">
+        <Notification title="使い方ヒント" text={notificationText} notificationType="info" />
         <TextField
           label="検索"
           variant="outlined"
@@ -30,7 +34,7 @@ export default function Search() {
         />
       </div>
       {Loading ? (
-        <h2>Loading...</h2>
+        <CircularProgress />
       ) : (
         ResponseJSON.flat().map((prop, index) => {
           if (prop.title.includes(searchText) || prop.tags[0].name.includes(searchText)) {
